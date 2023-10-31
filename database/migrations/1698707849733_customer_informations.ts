0d4bb6b17505af155d7048f1e4520e68044c678b
Daniel Bento <danielmayne2021@gmail.com>
2023-10-30 21:02:08 -0300

    Create Migration Customer Information

---
+++ database/migrations/1698707849733_customer_informations.ts
@@ -0,0 +1,26 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'customer_informations'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.text('title').notNullable()
+      table.text('text', 'longtext').notNullable()
+      table.enum('type', ['text', 'code', 'image']).notNullable()
+      table.text('language')
+      table.enum('status', ['active', 'deactivated']).defaultTo('active')
+      table.boolean('is_valid')
+      table.integer('customer_id').unsigned().notNullable().references('id').inTable('customers').onDelete('CASCADE')
+      table.integer('created_by').unsigned().notNullable().references('id').inTable('users').onDelete('CASCADE')
+      table.integer('update_by').unsigned().notNullable().references('id').inTable('users').onDelete('CASCADE')
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
