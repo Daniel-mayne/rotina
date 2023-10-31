65072ddec9b33891c81177ba50e356732cd9c3ae
Daniel Bento <danielmayne2021@gmail.com>
2023-10-30 22:20:28 -0300

    Create Validator Customer Information

---
+++ app/Validators/CustomerInformation/StoreValidator.ts
@@ -0,0 +1,19 @@
+import { schema, CustomMessages} from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export class StoreValidator {
+  constructor(protected ctx: HttpContextContract) { }
+
+  public schema = schema.create({
+    title: schema.string({ trim: true }),
+    text: schema.string({ trim: true }),
+    type: schema.enum(['text', 'code', 'image'] as const),
+    language: schema.string.optional({ trim: true }),
+    status: schema.enum.optional(['active', 'deactivated'] as const),
+    isValid: schema.string.optional({ trim: true })
+  })
+
+  public messages: CustomMessages = {
+    'required': 'O campo {{ field }} é obrigatório.',
+  }
+}
---
+++ app/Validators/CustomerInformation/UpdateValidator.ts
@@ -0,0 +1,19 @@
+import { schema, CustomMessages } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export class UpdateValidator {
+  constructor(protected ctx: HttpContextContract) { }
+
+  public schema = schema.create({
+    title: schema.string.optional({ trim: true }),
+    text: schema.string.optional({ trim: true }),
+    type: schema.enum.optional(['text', 'code', 'image'] as const),
+    language: schema.string.optional({ trim: true }),
+    status: schema.enum.optional(['active', 'deactivated'] as const),
+    isValid: schema.string.optional({ trim: true })
+  })
+
+  public messages: CustomMessages = {
+    'required': 'O campo {{ field }} é obrigatório.'
+  }
+}
