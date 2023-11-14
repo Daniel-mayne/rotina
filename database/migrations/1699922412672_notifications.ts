c5e8f0b229ef326110f85c2855844274a73654ab
Daniel Bento <danielmayne2021@gmail.com>
2023-11-13 21:51:55 -0300

    Create migration notifications.

---
+++ database/migrations/1699922412672_notifications.ts
@@ -0,0 +1,20 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'notifications'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('user_id').unsigned().notNullable().references('id').inTable('users').onDelete('CASCADE')
+      table.integer('approval_item_id').unsigned().notNullable().references('id').inTable('approval_items').onDelete('CASCADE')
+      table.integer('comment_id').unsigned().notNullable().references('id').inTable('post_comments').onDelete('CASCADE')
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
