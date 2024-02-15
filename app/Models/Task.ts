0a4c30509e2d4d745eef19070c408fb0422bd317
Daniel Bento <danielmayne2021@gmail.com>
2024-02-15 16:23:45 -0300

    Model Task

--- app/Models/Task.ts
+++ app/Models/Task.ts
@@ -5,9 +5,87 @@ export default class Task extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
-  @column.dateTime({ autoCreate: true })
+  @column()
+  public title: string
+
+  @column()
+  public currentUserId: number
+
+  @column()
+  public clientId: number
+
+  @column.dateTime({
+    serialize: (value: DateTime) => {
+      return value.toFormat('dd/MM/yyyy')
+    },
+  })
+  public dueDate: DateTime
+
+  @column()
+  public approvalUserId: number
+
+  @column.dateTime({
+    serialize: (value: DateTime) => {
+      return value.toFormat('HH:mm:ss')
+    },
+  })
+  public estimatedTime: DateTime
+
+  @column()
+  public taskTempateId: number
+
+  @column()
+  public taskDescription: string
+
+  @column()
+  public companyId: number
+
+  @column()
+  public status: 'waiting_approval' | 'approved' | 'disapproved' | 'deleted'
+
+  @column()
+  public projectId: number
+
+  @column()
+  public order: number
+
+  @column()
+  public initialUserId: number
+
+  @column()
+  public sentApprovalUserId: number
+
+  @column.dateTime({
+    serialize: (value: DateTime) => {
+      return value.toFormat('dd/MM/yyyy')
+    },
+  })
+  public sentApprovalDate: DateTime
+
+  @column.dateTime({
+    serialize: (value: DateTime) => {
+      return value.toFormat('dd/MM/yyyy')
+    },
+  })
+  public approvaDate: DateTime
+
+  @column()
+  public progress: number
+
+  @column.dateTime({
+    autoCreate: true,
+    serialize: (value: DateTime) => {
+      return value.toFormat('dd/MM/yyyy HH:mm:ss')
+    },
+  })
   public createdAt: DateTime
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  @column.dateTime({
+    autoCreate: true,
+    autoUpdate: true,
+    serialize: (value: DateTime) => {
+      return value.toFormat('dd/MM/yyyy HH:mm:ss')
+    },
+  })
   public updatedAt: DateTime
 }
