ee36bff781faa8d220673061691f8157759dbf3f
Daniel Bento <danielmayne2021@gmail.com>
2024-02-19 17:31:01 -0300

    migration Department

--- database/migrations/1708374111438_departments.ts
+++ database/migrations/1708374111438_departments.ts
@@ -3,19 +3,23 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class extends BaseSchema {
   protected tableName = 'departments'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
 
-      /**
-       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
-       */
+      table.string('name').notNullable()
+      table.integer('company_id').unsigned().references('id').inTable('companies')
+      table.enu('status', ['active', 'inactive']).notNullable().defaultTo('active')
+      table.integer('department_leader_id').unsigned().references('id').inTable('users')
+      table.integer('Department _coordinator_id').unsigned().references('id').inTable('users')
+      table.integer('userId').unsigned().references('id').inTable('users')
+
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
